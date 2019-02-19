fbcb0d266eb8b74cb1ca3f956c5e661603104867
Deividas Gedgaudas <sidicer@gmail.com>
2019-02-19 12:52:37 +0200

    Fixed Classes (//public variables)

--- main.cpp
+++ main.cpp
@@ -22,18 +22,32 @@ struct Coordinates {
 };
 
 class Snake {
-	public:
-		int direction,
-			headX,
-			headY,
-			tailX,
-			tailY;
+private:
+	int direction,
+		headX,
+		headY,
+		tailX,
+		tailY;
+public:
+	int GetHeadX() { return this->headX; }
+	int GetHeadY() { return this->headY; }
+	int GetDirection() { return this->direction; }
+
+	void SetHeadX(int x) { this->headX = x; }
+	void SetHeadY(int y) { this->headY = y; }
+	void SetDirection(int dir) { this->direction = dir; }
 };
 
 class Console {
-	public:
-		int width,
-			height;
+private:
+	int width,
+		height;
+public:
+	int GetWidth() { return this->width; }
+	int GetHeight() { return this->height; }
+
+	void SetWidth(int w) { this->width = w; }
+	void SetHeight(int h) { this->height = h; }
 };
 
 
@@ -48,76 +62,76 @@ void cursor_goto(int x, int y) {
 }
 
 // Function to pause the program while using it/playing
-void pause_for_user() { 
+void pause_for_user() {
 	std::string line;
 	std::cin >> line;
 }
- // Function to get the console window size by rows and columns (1 row/column = 1 pixel)
+// Function to get the console window size by rows and columns (1 row/column = 1 pixel)
 void get_console_window_size() {
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
 	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-	console.width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-	console.height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+	console.SetWidth(csbi.srWindow.Right - csbi.srWindow.Left + 1);
+	console.SetHeight(csbi.srWindow.Bottom - csbi.srWindow.Top + 1);
 }
 
 // Drawing the "walls" (outside border)
 void draw_window_outline(int score) {
 	_setmode(_fileno(stdout), 0x20000); // For ASCII to work
 
-	for (int i = 0; i < console.width - 8; i++) { // Top Border (-8 compensates for "S N A K E")
+	for (int i = 0; i < console.GetWidth() - 8; i++) { // Top Border (-8 compensates for "S N A K E")
 		if (i == 0)
 			wprintf(L"╔");
-		else if (i == console.width - 9) // -9 compensates for "S N A K E"
+		else if (i == console.GetWidth() - 9) // -9 compensates for "S N A K E"
 			wprintf(L"╗");
-		else if (i == console.width / 2)
+		else if (i == console.GetWidth() / 2)
 			wprintf(L"S N A K E");
 		else
 			wprintf(L"═");
 	}
 
-	cursor_goto(0, console.height - 2); // Bottom Border
-	for (int i = 0; i < console.width; i++) {
+	cursor_goto(0, console.GetHeight() - 2); // Bottom Border
+	for (int i = 0; i < console.GetWidth(); i++) {
 		if (i == 0)
 			wprintf(L"╚");
-		else if (i == console.width - 1)
+		else if (i == console.GetWidth() - 1)
 			wprintf(L"╝");
 		else
-			wprintf(L"═"); 
-	} 
+			wprintf(L"═");
+	}
 
 	cursor_goto(0, 1); // Left border
-	for (int i = 1; i < console.height - 1; i++) {
+	for (int i = 1; i < console.GetHeight() - 1; i++) {
 		wprintf(L"║");
 		cursor_goto(0, i);
-	} 
+	}
 
-	cursor_goto(console.width - 1, 1); // Right border
-	for (int i = 1; i < console.height - 1; i++) {
+	cursor_goto(console.GetWidth() - 1, 1); // Right border
+	for (int i = 1; i < console.GetHeight() - 1; i++) {
 		wprintf(L"║");
-		cursor_goto(console.width - 1, i);
-	} 
+		cursor_goto(console.GetWidth() - 1, i);
+	}
 
-	cursor_goto(1, console.height - 1);
-	wprintf(L"Score: %i",score * 10);
+	cursor_goto(1, console.GetHeight() - 1);
+	wprintf(L"Score: %i", score * 10);
 }
 
 void is_key_pressed() {
 	if (_kbhit() && _getch()) {
 		switch (_getch()) {
-			case 72: // code for arrow up
-			if (snake.direction != 2) snake.direction = 1;
+		case 72: // code for arrow up
+			if (snake.GetDirection() != 2) snake.SetDirection(1);
 			break;
 
-			case 80: // code for arrow down
-			if (snake.direction != 1) snake.direction = 2;
+		case 80: // code for arrow down
+			if (snake.GetDirection() != 1) snake.SetDirection(2);
 			break;
 
-			case 75: // code for arrow left
-			if (snake.direction != 4) snake.direction = 3;
+		case 75: // code for arrow left
+			if (snake.GetDirection() != 4) snake.SetDirection(3);
 			break;
 
-			case 77: // code for arrow right
-			if (snake.direction != 3) snake.direction = 4;
+		case 77: // code for arrow right
+			if (snake.GetDirection() != 3) snake.SetDirection(4);
 			break;
 		}
 	}
@@ -132,65 +146,65 @@ int main() {
 	draw_window_outline(score);
 
 	srand(time(NULL));
-	snake.direction = rand() % 4 + 1;
+	snake.SetDirection(rand() % 4 + 1);
 
-	snake.headX = console.width / 2;
-	snake.headY = console.height / 2;
+	snake.SetHeadX(console.GetWidth() / 2);
+	snake.SetHeadY(console.GetHeight() / 2);
 
 	srand(time(NULL));
-	fruit.X = rand() % (console.width - 2) + 1;
-	fruit.Y = rand() % (console.height - 3) + 1;
+	fruit.X = rand() % (console.GetWidth() - 2) + 1;
+	fruit.Y = rand() % (console.GetHeight() - 3) + 1;
 	cursor_goto(fruit.X, fruit.Y);
 	wprintf(L"@");
 
 	while (!gameOver) {
 		is_key_pressed();
 
-		switch (snake.direction) {
+		switch (snake.GetDirection()) {
 		case 1:
-			snake.headY--;
+			snake.SetHeadY(snake.GetHeadY()-1); // snakeHeadY--;
 			break;
 		case 2:
-			snake.headY++;
+			snake.SetHeadY(snake.GetHeadY()+1);
 			break;
 		case 3:
-			snake.headX--;
+			snake.SetHeadX(snake.GetHeadX()-1);
 			break;
 		case 4:
-			snake.headX++;
+			snake.SetHeadX(snake.GetHeadX()+1);
 			break;
 		}
 
-		if (snake.headX == console.width - 1) snake.headX = 1;
-		if (snake.headX == 0) snake.headX = console.width - 2;
-		if (snake.headY == console.height - 2) snake.headY = 1;
-		if (snake.headY == 0) snake.headY = console.height - 3;
+		if (snake.GetHeadX() == console.GetWidth() - 1) snake.SetHeadX(1);
+		if (snake.GetHeadX() == 0) snake.SetHeadX(console.GetWidth() - 2);
+		if (snake.GetHeadY() == console.GetHeight() - 2) snake.SetHeadY(1);
+		if (snake.GetHeadY() == 0) snake.SetHeadY(console.GetHeight() - 3);
 
-		if (snake.headX == fruit.X && snake.headY == fruit.Y) {
+		if (snake.GetHeadX() == fruit.X && snake.GetHeadY() == fruit.Y) {
 			wprintf(L" ");
 
 			score++;
-			cursor_goto(1, console.height - 1);
+			cursor_goto(1, console.GetHeight() - 1);
 			wprintf(L"Score: %i", score * 10);
 			Sleep(50);
 
 			srand(time(NULL));
-			fruit.X = rand() % (console.width - 2) + 1;
-			fruit.Y = rand() % (console.height - 3) + 1;
+			fruit.X = rand() % (console.GetWidth() - 2) + 1;
+			fruit.Y = rand() % (console.GetHeight() - 3) + 1;
 			cursor_goto(fruit.X, fruit.Y);
 			wprintf(L"@");
 		}
 
 		wprintf(L" ");
-		cursor_goto(snake.headX, snake.headY);
+		cursor_goto(snake.GetHeadX(), snake.GetHeadY());
 		wprintf(L"█");
-		cursor_goto(snake.headX, snake.headY);
-		
+		cursor_goto(snake.GetHeadX(), snake.GetHeadY());
+
 		Sleep(50);
 	}
 
 	_setmode(_fileno(stdout), _O_TEXT); // Reset from ASCII (to fix "std::cout")
 	std::cin.sync();
 	pause_for_user();
-    return 0;
+	return 0;
 }
